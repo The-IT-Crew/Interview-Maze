ee41af4e5cf602421b173bbd2acf9535edb5acce
suvasish114 <misuvasish114@gmail.com>
2022-08-01 12:23:29 +0530

    array rotation

---
+++ Problem Solving/array_rotation.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+// swap by reference
+void swap(int & a, int & b){    // time complexity = O(1), space = constant
+    int temp = a;
+    a = b;
+    b = temp;
+}
+
+// rotate the array
+void rotate(int * arr, int size){       // time complexity = O(n)
+    int half = size/2;                  // time = constant, space = constant
+    for (int i=0; i<half; i++){         // time = n
+        swap(arr[i], arr[size-i-1]);    // time = constant
+    }
+}
+
+// driving code
+int main(){
+    int arr[] = {1,2,3,4,5,6,7,8,9,10};     // array initialization
+    rotate(arr, 10);                        // roate the array
+    for (int i=0; i<10; i++){
+        cout<< arr[i] <<" ";
+    }
+    cout<<endl;
+    return 0;
+}
