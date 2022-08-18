5a0fcaea8a37b8a6746dc63132cffe9678c42395
suvasish114 <misuvasish114@gmail.com>
2022-08-18 14:49:19 +0530

    stack using static array

--- Data Structures/Stack/stack.cpp
+++ Data Structures/Stack/stack.cpp
@@ -1,47 +1,69 @@
-// implementation of stack (LIFO) using sigle linked list (std::forward_list)
+// implementation of stack (LIFO) using fix size array (std::array)
 
 #include <iostream>
-#include <list>                         // single linked list, standard template library
+#define SIZE 10                                     // size of the stack strucure
 using namespace std;
 
 class Stack {
-    list<int> list;                     // stack
-    public:
-    void push (int);                    // insert data at the end of the list
-    int pop ();                         // delete data from the top
-    void display ();                    // display the list
+    int stack[SIZE];                                // fix size array 
+    int TOP;                                        // initialy pointing to imaginary location
+    public: 
+
+    Stack() {                                       // default constructor
+        TOP = -1;
+    }
+
+    void push (int data) {                          // push data element into stack
+        if (TOP == SIZE-1) {                        // if stack is full
+            cout << "stack is full" << endl;
+            return;
+        }                                           // if stack is not full
+        TOP += 1;
+        stack [TOP] = data;
+    }
+
+    int pop () {                                    // pop data element from stack
+        if (TOP == -1) {                            // stack is empty
+            cout << "stack is empty" << endl;
+            exit(0);
+        }                                           // if stack is not empty
+        int data = stack[TOP];
+        TOP -= 1;
+        return data;
+    }
+
+    int top () {                                    // return top elent of the stack
+        if (TOP == -1) {                            // if stack is empty
+            cout << "stack is empty" << endl;
+            exit(0);
+        }                                           // else
+        return stack[TOP];
+    }
+
+    void display () {                               // display the elements of the stack
+        if (TOP == -1) {                            // if stack is empty
+            cout << "stack is empty" << endl;
+            return;
+        }       
+        for (int i=0; i<=TOP; i++) {                // else
+            cout << stack[i] << " ";
+        }
+        cout << endl;
+    }
 };
 
-int main(){
-    Stack list;
-    list.display();                     // empty list
-    list.push(10);
-    list.push(20);
-    list.push(30);
-    list.display();                     // [10, 20, 30]
-    list.pop();
-    list.display();                     // [10, 20]
+// driving code
+int main () {
+    Stack stack;
+    stack.display();
+    stack.push(10);
+    stack.push(20);
+    stack.display();
+    stack.push(30);
+    stack.push(40);
+    stack.display();
+    cout << "delete: " << stack.pop() << endl;
+    cout << "top: " << stack.top() << endl;
+    stack.display();
     return 0;
-}
-
-void Stack::push (int data) {
-    list.push_back(data);               // insert data at the end of the linked list
-}
-
-int Stack::pop () {                     // delete element from the top
-    if (list.empty() == true) {
-        cout << "stack underflow! can not delete element" << endl;
-        return;
-    }
-    int back = list.back();
-    list.pop_back();                    // delete the last element
-    return back;
-}
-
-void Stack::display () {                // display stack elements
-    if (list.empty() == true) {
-        cout << "empty stack! can not display" << endl;
-        return;
-    }
-    cout << "Stack elements: " << list << endl;
 }
