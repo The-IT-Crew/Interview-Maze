53b748bb45ee3e570df2612e8e7b816f06037540
SUVASISH DAS <misuvasish114@gmail.com>
2022-09-23 09:45:20 +0530

    Update missing_element.cpp

--- Problem Solving/miscellaneous/missing_element.cpp
+++ Problem Solving/miscellaneous/missing_element.cpp
@@ -4,12 +4,12 @@
 // should give 2. The input [1, 2, 0] should give 3.
 
 #include <iostream>
-#include <unordered_set>							// unordered set
+#include <unordered_set>					// unordered set
 using namespace std;
 
 int missing_element (int * arr, int size) {			// time complexity: O(n), space complexity: O(n)
-	unordered_set <int> set;						// space: n
-	for (int i=0; i<size; i++) {					// time: n
+	unordered_set <int> set;				// space: n
+	for (int i=0; i<size; i++) {				// time: n
 		if (arr[i] >= 0) {
 			set.insert(arr[i]);
 		}
@@ -18,19 +18,19 @@ int missing_element (int * arr, int size) {			// time complexity: O(n), space co
 	min = temp = *set.begin();
 	auto itr = set.begin();
 	itr++;
-	while (itr != set.end()) {						// time: n
-		if(*itr != temp+1){							// if, missing element is in between
+	while (itr != set.end()) {				// time: n
+		if(*itr != temp+1){				// if, missing element is in between
 			return temp+1;
 		} else {
 			temp = *itr;
 		}
 		itr++;
 	}
-	if (min-1 >= 0) return min-1;					// if, missing element is at the start
-	return temp+1;									// if, missing element is at the end
+	if (min-1 >= 0) return min-1;				// if, missing element is at the start
+	return temp+1;						// if, missing element is at the end
 }
 
-int main() {										// driving code
+int main() {							// driving code
 	int arr[] = {2,3,1};
 	cout << "hello world" << endl;
 	cout << missing_element(arr, 3) << endl;		// function call
