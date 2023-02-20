270fee686d58dfd6b20a958d968425e45926c732
SUVASISH DAS <misuvasish114@gmail.com>
2023-02-20 17:47:26 +0530

    meta added

--- Problem Solving/Dynamic Programming/coin_change.py
+++ Problem Solving/Dynamic Programming/coin_change.py
@@ -1,7 +1,14 @@
 # COIN CHANGE PROBLEM
-# Problem Link:
-# Problem Statement:
-# Examples:
+# Problem Link: https://leetcode.com/problems/coin-change/
+# Problem Statement: You are given an integer array coins representing coins of different denominations and an integer
+# amount representing a total amount of money. Return the fewest number of coins that you need to make up that amount.
+# If that amount of money cannot be made up by any combination of the coins, return -1. You may assume that you have an
+# infinite number of each kind of coin.
+
+# Example:
+# Input: coins = [1,2,5], amount = 11
+# Output: 3
+# Explanation: 11 = 5 + 5 + 1
 
 def coin_change(coins, amount):
     dp = [[amount+1 for _ in range(amount+1)] for _ in range(len(coins)+1)]
