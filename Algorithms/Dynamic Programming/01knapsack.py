faca00a9941160c729346bfb622d27709399c1a2
SUVASISH DAS <misuvasish114@gmail.com>
2023-02-20 14:11:46 +0530

    updated knapsack problem

--- Algorithms/Dynamic Programming/01knapsack.py
+++ Algorithms/Dynamic Programming/01knapsack.py
@@ -1,42 +1,27 @@
-# 01 knapsack using dynamic programming
-# find maximum betweeen two numbers
-def max(a, b):
-    if a>b:
-        return a
-    return b
+# 01 knapsack
+# Find the maximum profit
 
-# sort the inputs according to their weights
-def sort_weights(weights, profits):
-    if weights != sorted(weights.copy()):
-        for i in range(len(weights)-1):
-            if weights[i] > weights[i+1]:
-                # swap
-                weights[i], weights[i+1] = weights[i+1], weights[i]
-                profits[i], profits[i+1] = profits[i+1], profits[i]
+def knapsack(weights, profits, capacity, total_items):
+    dp = [[0 for _ in range(capacity+1)] for _ in range(total_items+1)]
 
-# knapsack funciton
-def knapsack(weights, profits, capacity, table):
-    # code
-    for i in range(1, len(weights)+1):
-        for j in range(1, capacity+1):
-            # fourmula
-            if weights[i-1] <= j:
-                table[i][j] = max(profits[i-1]+table[i-1][j-weights[i-1]], table[i-1][j])
+    for i in range(1,total_items+1):
+        for j in range(1,capacity+1):
+            if j >= weights[i-1]:
+                dp[i][j] = max(dp[i-1][j], dp[i-1][j-weights[i-1]]+profits[i-1])
             else:
-                table[i][j] = table[i-1][j]
-    return table
+                dp[i][j] = dp[i-1][j]
+
+    for a in dp:
+        print(a)
+
+    # TODO: print the items included in knapsack
+
+    return dp[-1][-1]
 
 # testing
-if __name__ == '__main__':
-    weights = list(map(int, input('Enter the weights: ').strip().split(' ')))
-    profits = list(map(int, input('Enter the profits: ').strip().split(' ')))
-    capacity = int(input('Enter the capacity: ').strip())
-    selected_indexes = [0 for _ in range(len(weights))]
-    table = [[0 for _ in range(capacity+1)] for _ in range(len(weights)+1)]
-    sort_weights(weights, profits)
-    table = knapsack(weights, profits, capacity, table)
-    for a in table:
-        for b in a:
-            print(f'{b:>3}', end='')
-        print()
-    print(f"Maximum profit is: {table[len(weights)][capacity]}")
+if __name__ == "__main__":
+    item_weights = [10,8,4,9,5]
+    item_profits = [4,5,8,4,7]
+    n = 5
+    c = 20
+    print(knapsack(item_weights, item_profits,c,n))
