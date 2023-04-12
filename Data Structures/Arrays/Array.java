ce81a01a082d1a6764d788251e6dc04ff961b5dc
Suvasish Das <misuvasish114@gmail.com>
2023-04-12 07:56:33 +0530

    array using java

---
+++ Data Structures/Arrays/Array.java
@@ -0,0 +1,10 @@
+class Array{
+    public static void main(String args[]){
+        
+        final int SIZE = 10;
+        int myArray[];                      // array initialization
+        myArray = new int[SIZE];            // array declaration
+        
+        int myArray2[] = {1,2,3,4};         // inline array creation
+    }
+}
